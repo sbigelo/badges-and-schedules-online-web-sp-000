c2ea153bdb415d7f206264c0e98baa156e7134a2
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-05 21:29:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(names)
+  "Hello, my name is #{names}."
+end
+
+def batch_badge_creator(names)
+   names
+   
+end
+
+def assign_rooms(list)
+  list.each_with_index.map do |num, index|
+  "Hello, #{num}! You'll be assigned to room #{index + 1}!"
+  
+end
+end
